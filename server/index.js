d1291b2eb3f3a50e7c091f990442bf3c1574e793
Lucille-R <lucillerichard37@yahoo.fr>
2026-08-26 23:41:55 +0200

    Fais l'ex 2 index.js et routes/objets.js

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config"; // Va servir à importer le numéro du port (3000) au lieu de l'écrire en dur dans app.listen(3000, ...)
+import objetsRouter from "./routes/objets.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/objets", objetsRouter);
+
+app.listen(process.env.PORT, () => {
+	console.log("Serveur sur http://localhost:3000");
+});
