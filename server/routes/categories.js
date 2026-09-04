af84814c6f091a48d0e4b4f821a49124783d7fe3
Lucille-R <lucillerichard37@yahoo.fr>
2026-09-04 15:50:41 +0200

    Monte la route get categorie

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -2,3 +2,10 @@ import express from "express";
 import { pool } from "../db.js";
 
 export const routerCategories = express.Router();
+
+routerCategories.get("/categories", async (req, res) => {
+	const { rows } = await pool.query(`
+		SELECT id, libelle
+		FROM categorie`);
+	res.status(200).json(rows);
+});
